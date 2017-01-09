8af680dba5ec132e278503aab62b00268ee3047d
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-01-09 10:21:10 -0600

    Initial Commit

---
+++ TicTacToe/GridLabel.swift
@@ -0,0 +1,17 @@
+//
+//  GridLabel.swift
+//  TicTacToe
+//
+//  Created by Andrew James Kinsey on 11/18/16.
+//  Copyright © 2016 Andrew Kinsey. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel
+{
+
+    var xTurn = true
+    var canTap = true
+    var count = 0
+}
